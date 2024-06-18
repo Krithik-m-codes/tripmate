d7656acbec0515a4572433e466295aedd7529a67
Krithik M <krithikm923@gmail.com>
2024-06-18 11:24:20 +0530

    feat: Add dashboard and profile pages for authenticated users

---
+++ src/app/(auth)/dashboard/page.tsx
@@ -0,0 +1,36 @@
+// import React from 'react';
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+
+// export const metadata = {
+//     title: "Dashboard",
+//     description: "Dashboard",
+// };
+
+const Dashboard = () => {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <pre>
+          {JSON.stringify(session)}
+          <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </pre>
+      </>
+    );
+  }
+
+  return (
+    <pre>
+      {session}
+      <br />
+      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signOut()}>Sign out</button>
+    </pre>
+  );
+};
+
+export default Dashboard;
